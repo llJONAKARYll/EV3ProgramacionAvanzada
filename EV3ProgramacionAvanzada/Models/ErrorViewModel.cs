e28d2ad3e660ee61d9f771aadeb9d3191ce6c6b1
llJONAKARYll <joelr.3214@gmail.com>
2024-10-21 11:34:43 -0300

    Agregar archivos de proyecto.

---
+++ EV3ProgramacionAvanzada/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EV3ProgramacionAvanzada.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
